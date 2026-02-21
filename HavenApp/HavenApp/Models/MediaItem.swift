756ac40c01c1f2b89e91b3173000e15b8438fd4f
BTCforPlebs <btcforplebs@gmail.com>
2026-02-21 08:28:49 -0500

    feat: show mime type for media items in viewer
    Use NIP-94 "m" tag, magic byte detection, and URL extension mapping
    to capture and display mime types. Unknown files now show their
    detected mime type instead of a generic label.

--- HavenApp/HavenApp/Models/MediaItem.swift
+++ HavenApp/HavenApp/Models/MediaItem.swift
@@ -7,7 +7,8 @@ struct MediaItem: Identifiable, Codable {
     let dateAdded: Date
     let pubkey: String? // Author of the event containing this media
     let tags: [[String]]? // Tags of the event containing this media
-    
+    let mimeType: String?
+
     enum MediaType: String, Codable {
         case image
         case video
